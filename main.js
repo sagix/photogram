ff2af13d60db568c6e49a808d1ea8127d9081a15
Nicolas Mouchel <nmo@octo.com>
2015-07-12 16:48:00 +0200

    add i18n

--- main.js
+++ main.js
@@ -4,6 +4,16 @@ window.addEventListener('load', function() {
     data.init(',', '"');
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    Array.prototype.forEach.call(document.querySelectorAll('[data-i18n]'), function(el) {
+        el.textContent = chrome.i18n.getMessage(el.getAttribute('data-i18n'));
+    });
+    Array.prototype.forEach.call(document.querySelectorAll('[data-i18n-attr]'), function(el) {
+        var data = el.getAttribute('data-i18n-attr').split(':');
+        el.setAttribute(data[0], chrome.i18n.getMessage(data[1]));
+    });
+});
+
 function loadDirEntry(chosenEntry) {
     if (chosenEntry !== undefined && chosenEntry.isDirectory) {
         var dirReader = chosenEntry.createReader();
