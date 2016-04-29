adf0a2995a63a2eaab27d0cd6c3b3a4779b04c07
Nicolas Mouchel <nmo@octo.com>
2016-04-29 08:57:27 +0200

    add open directory message

--- keys.js
+++ keys.js
@@ -1,9 +1,14 @@
 window.addEventListener('keydown', function(event) {
     if (event.keyCode === 27) { //escape
         form.close();
-    } else if (event.metaKey && event.keyCode == 80) { //cmd+P
+    } else if (cmd(event, 79)) { //cmd+O
+        elements.chooseDir();
+    } else if (cmd(event, 80)) { //cmd+P
         window.print();
-    } else if (event.metaKey && event.keyCode == 13) { //cmd+enter
+    } else if (cmd(event, 13)) { //cmd+enter
         form.submit();
+    }
+    function cmd(event, key){
+        return event.metaKey && event.keyCode == key
     }
 });
